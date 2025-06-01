97e514b63e66a9391ceebcef3d531553039a47ee
alireza <alireza.vb1995@gmail.com>
2025-06-01 12:09:09 +0330

    Update 8-file_read_write.c

--- 8-file_read_write.c
+++ 8-file_read_write.c
@@ -4,18 +4,16 @@ int main(void) {
     FILE *file;
     char text[100];
 
-    // Write to file
     file = fopen("myfile.txt", "w");
     if (file == NULL) {
         printf("Error opening file!\n");
         return 1;
     }
     printf("Enter text to write to file: ");
-    scanf(" %[^\n]", text); // read line with spaces
+    scanf(" %[^\n]", text);
     fprintf(file, "%s\n", text);
     fclose(file);
 
-    // Read from file
     file = fopen("myfile.txt", "r");
     if (file == NULL) {
         printf("Error opening file!\n");
